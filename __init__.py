02ff742324c27fce7359aba8f17990e710bfce62
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2024-12-30 17:14:38 +0000

    somme

--- __init__.py
+++ __init__.py
@@ -20,3 +20,9 @@ def MaPremiereAPI():
 @app.route('/calcul_carre/<int:val_user>')
 def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
+
+@app.route('/somme/<int:valeur1>/<int:valeur2>')
+def somme(valeur1, valeur2):
+    result = valeur1 + valeur2
+    return f"<h2>La somme de vos valeurs est : </h2>{result}"
+
